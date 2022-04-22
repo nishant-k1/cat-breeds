da1ef4fa032554e1a1decd03df6ef3e377772f82
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 05:15:49 +0530

    create a HomePage component

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CatBreeds from '../../components/CatBreeds/CatBreeds'
+
+const HomePage = () => {
+  return (
+    <CatBreeds />
+  )
+}
+
+export default HomePage
