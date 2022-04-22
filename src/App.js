8f6145bad604d0a7b2d210681b745c9ead11a1df
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 05:06:47 +0530

    Create Layout component and AppBar components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import AppBar from './components/AppBar/AppBar';
 import './App.css';
+import { Stack } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack className=".App">
+        <AppBar />
+        <Layout />
+    </Stack>
   );
 }
 
