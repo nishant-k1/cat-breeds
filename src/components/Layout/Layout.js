1233085e489db7a278558ba611b98bb6989c7daf
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 22:10:03 +0530

    customize layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,36 +1,14 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import HomePage from '../../pages/HomePage/HomePage'
-import { createTheme } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
-import {
-  Container
-} from '@mui/material';
+import HomePage from '../../pages/HomePage/HomePage';
+import AppBar from '../AppBar/AppBar'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-});
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
-  const Layout = () => {
-    return (
-        <HomePage />
-    );
-  }
+const Layout = () => {
+  return (
+    <React.Fragment>
+      <AppBar />
+      <HomePage />
+    </React.Fragment>
+  );
+}
 
 export default Layout
