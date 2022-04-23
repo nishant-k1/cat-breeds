e896cc90fbebe2bbd6cd1d587cad00345e5b0b50
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 22:10:27 +0530

    implement pagination logic

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,17 +3,21 @@ import Typography from '@mui/material/Typography';
 import PaginationMui from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-const Pagination = () => {
-    const [page, setPage] = React.useState(1);
-
+const Pagination = ({page, setPage, catCountPerPage}) => {
     const handleChange = (event, value) => {
       setPage(value);
     };
 
     return (
-        <Stack spacing={2} >
-            <Typography>Page: {page}</Typography>
-            <PaginationMui count={25} page={page} onChange={handleChange} />
+        <Stack spacing={2}>
+            {/* <Typography>Page: {page}</Typography> */}
+            <PaginationMui
+                count={catCountPerPage}
+                page={page}
+                onChange={handleChange}
+                showFirstButton
+                showLastButton
+            />
         </Stack>
     );
 
