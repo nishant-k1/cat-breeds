bc8461b9c45e08bc331056481fc55044a1c58013
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 22:04:17 +0530

    customize AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,91 +1,90 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import '../../App.css';
 import { Container } from '@mui/material';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase
+} from './search'
 
 const NavBar = () => {
   return (
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <AppBar position="static" >
+    //     {/* <Container> */}
+    //     <Toolbar sx={{ maxHeight:"2rem", justifyContent:'space-between'}}>
+    //         <Typography
+    //           variant="h6"
+    //           noWrap
+    //           component="div"
+    //           // sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+    //         >
+    //           <h2 style={{fontFamily:"'Dancing Script','cursive'"}}>Cat Breeds</h2>
+    //         </Typography>
+    //         <Search >
+    //           <SearchIconWrapper>
+    //             <SearchIcon />
+    //           </SearchIconWrapper>
+    //           <StyledInputBase
+    //             placeholder="Search…"
+    //             inputProps={{ 'aria-label': 'search' }}
+    //           />
+    //         </Search>
+    //         <IconButton
+    //             size="large"
+    //             edge="start"
+    //             color="inherit"
+    //             aria-label="open drawer"
+    //             // sx={{ mr: 2 }}
+    //           >
+    //           <Brightness7Icon />
+    //         </IconButton>
+    //     </Toolbar>
+    //     {/* </Container> */}
+    //   </AppBar>
+    // </Box>
+
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" >
+      <AppBar position="static">
         <Container>
-        <Toolbar style={{height:"2rem"}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            <h2 style={{fontFamily:"'Dancing Script','cursive'"}}>Cat Breeds</h2>
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Toolbar>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              Cat Breeds
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-haspopup="true"
+                color="inherit"
+              >
+              </IconButton>
+            </Box>
+          </Toolbar>
         </Container>
       </AppBar>
     </Box>
