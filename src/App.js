bf4435b212a78879dabac3ea8d93ca8262d7263c
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-23 22:01:25 +0530

    remove AppBar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
-import AppBar from './components/AppBar/AppBar';
 import './App.css';
 import { Stack } from '@mui/material';
 
 function App() {
   return (
     <Stack className=".App">
-        <AppBar />
         <Layout />
     </Stack>
+
   );
 }
 
