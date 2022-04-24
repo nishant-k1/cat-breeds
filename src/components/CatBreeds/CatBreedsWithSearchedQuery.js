89417e4e5c1aa4d938b3eb96b693086f89e74ada
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-24 13:29:15 +0530

    dynamic search with conditional pagination

--- src/components/CatBreeds/CatBreedsWithSearchedQuery.js
+++ src/components/CatBreeds/CatBreedsWithSearchedQuery.js
@@ -14,41 +14,52 @@ const CatBreedsWithSearchedQuery = ({searchQuery}) => {
     const [page, setPage] = React.useState(1);
     const catBreedsList = useFetch(cat_breed_url, page);
     const [pageCount, setPageCount] = React.useState(null);
-    const [catBreeds, setCatBreeds] = React.useState([]);
     const [catBreedsSearched, setCatBreedsSearched] = React.useState([]);
+    const [catBreeds, setCatBreeds] = React.useState([]);
 
     React.useEffect(() => {
         if(catBreedsList.length > 0){
-            const itemCount = 9;
-            setPageCount(() => Math.ceil(catBreedsList.length/itemCount));
-            const last_item_index = page * itemCount;
-            const first_item_index = last_item_index - itemCount;
-            setCatBreeds(() => catBreedsList.slice(first_item_index, last_item_index));
-
-            setCatBreedsSearched(() => (catBreeds.filter(item => {
+            setCatBreedsSearched(() => (catBreedsList.filter(item => {
                 if(item.name.replace(/\s/g, "").trim().toLowerCase()
                     .includes(searchQuery.replace(/\s/g, "").trim().toLowerCase())){
                     return item
                 }
                 return null
-            })))
+            })));
         }
+
     }, [
         catBreedsList,
         catBreedsList.length,
+        searchQuery,
         pageCount,
         page,
         catBreeds,
         catBreeds.length,
-        searchQuery
     ]);
 
+    React.useEffect(() => {
+        const itemCount = 9;
+        setPageCount(() => Math.ceil(catBreedsList.length/itemCount));
+        const last_item_index = page * itemCount;
+        const first_item_index = last_item_index - itemCount;
+        setCatBreeds(() => catBreedsSearched.slice(first_item_index, last_item_index));
+    }, [
+        catBreedsList,
+        catBreedsList.length,
+        searchQuery,
+        catBreedsSearched,
+        catBreedsSearched.length,
+        pageCount,
+        page,
+        catBreeds,
+        catBreeds.length,
+    ]);
 
     return (
         <React.Fragment>
             {
-                // !catBreedList[0]
-                !(catBreedsSearched.length > 0)
+                !catBreedsSearched.length > 0
                     &&  <Container sx={
                             {
                                 display:"grid",
@@ -60,8 +71,8 @@ const CatBreedsWithSearchedQuery = ({searchQuery}) => {
                             <Loader />
                         </Container>
             }
-            {
-                !catBreedsSearched
+            {/* {
+                !catBreedsSearched[0]
                     &&  <Container sx={
                             {
                                 display:"grid",
@@ -77,9 +88,62 @@ const CatBreedsWithSearchedQuery = ({searchQuery}) => {
                                 Sorry!! No results found 😭
                             </Typography>
                         </Container>
+            } */}
+            {
+                catBreedsSearched.length > 0 && catBreedsSearched.length < 9
+                    &&  <Container>
+
+                                <Grid
+                                    container
+                                    spacing={4}
+                                    sx={{margin:"4rem 0"}}
+                                >
+                                    {
+                                        catBreedsSearched.map(item => {
+                                            return (
+                                                <React.Fragment key={item.id}>
+                                                    {
+                                                        item.image
+                                                            &&  <Grid
+                                                                    item
+                                                                    xs={12}
+                                                                    sm={6}
+                                                                    md={4}
+                                                                >
+                                                                    <Card sx={{ maxWidth: 345 }}>
+                                                                        <CardActionArea>
+                                                                            <CardMedia
+                                                                                component="img"
+                                                                                height="140"
+                                                                                image={item.image.url}
+                                                                                alt={item.name}
+                                                                            />
+                                                                        <CardContent>
+                                                                            <Typography
+                                                                                gutterBottom
+                                                                                variant="h5"
+                                                                                component="div">
+                                                                                {item.name}
+                                                                            </Typography>
+                                                                            <Typography
+                                                                                variant="body2"
+                                                                                color="text.secondary">
+                                                                                {item.description}
+                                                                            </Typography>
+                                                                        </CardContent>
+                                                                        </CardActionArea>
+                                                                    </Card>
+                                                                </Grid>
+                                                    }
+                                                </React.Fragment>
+                                            )
+                                        })
+                                    }
+                                </Grid>
+                        </Container>
             }
             {
-                catBreedsSearched.length > 0
+                catBreedsSearched.length > 9
                     &&  <Container>
                             <Grid
                                 container
@@ -106,7 +170,7 @@ const CatBreedsWithSearchedQuery = ({searchQuery}) => {
                                     spacing={4}
                                 >
                                     {
-                                        catBreedsSearched.map(item => {
+                                        catBreeds.map(item => {
                                             return (
                                                 <React.Fragment key={item.id}>
                                                     {
