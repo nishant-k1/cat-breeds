06e7975da5fcf1379c926133db203abe068d34d5
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-25 03:51:38 +0530

    customize custome hook to handle empty response and network error fallbacks

--- src/customHooks/useFetch.js
+++ src/customHooks/useFetch.js
@@ -1,26 +1,48 @@
 import React from 'react';
 
-
 // Creating one time custome hook using useEffect hook to fetch data from network
-export const useFetch = (URL, PAGE) => {
+export const useFetch = (url, page) => {
     // State to control the list data fetched from server
-    const [data, setData] = React.useState([]);
+    const [data, setData] = React.useState({
+        loading:true,
+        error:false,
+        list:[
+
+        ]
+    });
 
     React.useEffect(() => {
+        setData({
+            loading:true,
+            error:false,
+            list:[
+
+            ]
+        })
         try {
             (
                 async () => {
-                    const response = await fetch(URL, {
-                        method: 'GET'
-                    });
-                    // setting latest data on every new render
-                    setData(await response.json())
+                    const response = await fetch(url);
+                    const json_data = await response.json();
+
+                    // setting new state of fetched server data on every new render
+                    setData((state) => ({
+                        ...state,
+                        loading:false,
+                        list:[
+                            ...json_data
+                        ]
+                    }))
                 }
             )();
         } catch (error) {
-            console.log(error)
+            setData(async (state) => ({
+                ...state,
+                loading:false,
+                error:true,
+            }))
         }
-    }, [PAGE]);
+    }, [page, url]);
 
     return data
 }
