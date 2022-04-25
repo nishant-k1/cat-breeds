476b6870a0070d2867803a571abb5c345c2b64f1
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-25 06:07:47 +0530

    disable dark pallete

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React from 'react';
 import Layout from './components/Layout/Layout';
 import './App.css';
 import { Stack } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 function App({searchQuery, setSearchQuery}) {
 
-
   return (
     <React.Fragment>
-        <ThemeProvider theme={darkTheme}>
-          <Stack className=".App">
-            <Layout />
-          </Stack>
-        </ThemeProvider>
-
+      <Stack className=".App">
+        <Layout />
+      </Stack>
     </React.Fragment>
 
   );
