d428c14d0a92e1e20a249bbc3e6f833c9ef4be3b
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-25 08:10:31 +0530

    minor change in Loader UI

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Stack from '@mui/material/Stack';
 import CircularProgress from '@mui/material/CircularProgress';
-
+import { Container } from '@mui/material';
 // A Loader component to show while page data is still being fetched from the server
 const Loader = () => {
   return (
+    <Container sx={{
+      display:"grid",
+      justifyContent:"center",
+      alignItems:'center',
+      height:'80vh'
+    }}
+    >
     <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
         <CircularProgress color="secondary" />
-    </Stack>
+      </Stack>
+    </Container>
   )
 }
 
