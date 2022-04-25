cd2f082d33a127de34568f2ab43ed547fb92819b
Nishant Kumar <nishant290195.nkr@gmail.com>
2022-04-25 20:44:15 +0530

    remove audio

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import CatBreeds from '../../components/CatBreeds/CatBreeds'
-import meowSong from '../../assets/meow-cut.mp3';
 
 const HomePage = ({searchQuery}) => {
   // Hompage components
 
   return (
     <React.Fragment>
-      <audio
-        controls
-        autoPlay
-        style={{
-            position:'fixed',
-            bottom:'0',
-            right:'0',
-            width:"16rem",
-            background:"pink"
-          }}
-        >
-        <source
-          src={meowSong}
-          type="audio/ogg" />
-          Your browser does not support the audio element.
-      </audio>
       <CatBreeds searchQuery={searchQuery} />
     </React.Fragment>
   )
